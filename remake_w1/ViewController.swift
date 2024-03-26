ba169a170e88c7cf57c8e9f640fbce392f99dc3d
hieuvm <hieuvm.nws@gmail.com>
2024-03-26 15:59:34 +0700

    Initial Commit

---
+++ remake_w1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  remake_w1
+//
+//  Created by HieuVM3 on 26/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ remake_w1UITests/remake_w1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  remake_w1UITestsLaunchTests.swift
+//  remake_w1UITests
+//
+//  Created by HieuVM3 on 26/03/2024.
+//
+
+import XCTest
+
+final class remake_w1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
